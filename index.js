06c5af8beb4caaeb82349dd9a57f86f5dd9044fe
NihushaganS <s22010005@ousl.lk>
2024-11-13 08:09:13 +0530

    get book added

--- index.js
+++ index.js
@@ -12,3 +12,39 @@ app.get("/", (req, res) => {
 app.listen(PORT, () => {
   console.log("Server is running on post ${PORT}");
 });
+
+// Get all Books
+app.get("/", async (req, res) => {
+  const getAllBooks = await BookModel.find();
+  return res.json(getAllBooks);
+});
+
+// get book by id
+app.get("/is/:isbn", async (req, res) => {
+  const getSpecificBook = await BookModel.findOne({ ISBN: req.params.isbn });
+
+  //null !0 = 1 , !1=0
+  if (!getSpecificBook) {
+    return res.json({
+      error: `No book found for the ISBN of ${req.params.isbn}`,
+    });
+  }
+
+  return res.json({ book: getSpecificBook });
+});
+
+// get book by category
+app.get("/c/:category", async (req, res) => {
+  const getSpecificBook = await BookModel.findOne({
+    category: req.params.category,
+  });
+
+  //null !0 = 1 , !1=0
+  if (!getSpecificBook) {
+    return res.json({
+      error: `No book found for the category of ${req.params.category}`,
+    });
+  }
+
+  return res.json({ book: getSpecificBook });
+});
